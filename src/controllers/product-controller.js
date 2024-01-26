a2e3be03172ca42d017b143a7a87badb1e97312c
nawaphondev <nawaphonphophom55@gmail.com>
2024-01-26 19:53:41 +0700

    lasted

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,13 +1,13 @@
 exports.getProductsLanding = (req, res, next) => {
-    res.json({message: "Get Products Landing Page"});
+  res.json({ message: "Get Products Landing Page" });
 };
 
 exports.getProducts = (req, res, next) => {
-    const { search, promotion, brand, category } = req.query;
-    res.json({ search, promotion, brand, category })
+  const { search, promotion, brand, category } = req.query;
+  res.json({ search, promotion, brand, category });
 };
 
-exports.getProductById = (req ,res, next) => {
-    const { productId } = req.query;
-    res.json({  productId });
-}
+exports.getProductById = (req, res, next) => {
+  const { productId } = req.params;
+  res.json({ productId });
+};
